5b26b486b1f7d98fabfab6220589474a21a9d71e
Dmitry <dimansharonov95@gmail.com>
2024-02-14 19:25:41 +0300

    7 задание доделано

--- task7_rational_numbers.cpp
+++ task7_rational_numbers.cpp
@@ -2,8 +2,12 @@
 #include <cmath>
 #include <cassert>
 #include <sstream>
+#include <vector>
 
-int gcd(int a, int b) {
+using NumberType = int64_t;
+
+template<typename T>
+T gcd(T a, T b) {
     int t;
     while (b != 0) {
         t = b;
@@ -15,117 +19,163 @@ int gcd(int a, int b) {
 }
 
 
+template<typename T>
+T BinaryPower(T b, uint64_t e) {
+    T v = 1;
+    while (e != 0) {
+        if ((e & 1) != 0) {
+            v *= b;
+        }
+        b *= b;
+        e >>= 1;
+    }
+    return v;
+}
+
 class Rational {
 public:
-    Rational(int m, int n) : _numenator(m), _denomenator(n) {}
 
-    explicit Rational(int m) : _numenator(m), _denomenator(1) {}
+    Rational(NumberType m, NumberType n) : _numerator(m), _denominator(n) {}
+
+    // Фикс бед компиляции
+    Rational(int m, int n) : _numerator(m), _denominator(n) {}
+
+    Rational(NumberType n) : _numerator(n), _denominator(1) {}
 
     explicit Rational(double number, double precision) {
-        _numenator = static_cast<int>(number * precision);
-        _denomenator = static_cast<int>(precision);
-        reduction();
+        _numerator = static_cast<NumberType>(number * precision);
+        _denominator = static_cast<NumberType>(precision);
+        reduce();
     }
 
-    Rational() : _numenator(0), _denomenator(1) {}
+
+    Rational() : _numerator(0), _denominator(1) {}
 
     friend std::ostream &operator<<(std::ostream &out, const Rational &r) {
-        out << r._numenator << "/" << r._denomenator;
+        out << r._numerator << "/" << r._denominator;
         return out;
     }
 
     Rational operator-() const {
-        return {-_numenator, _denomenator};
+        return {-_numerator, _denominator};
     }
 
+    // prefix increment
     Rational &operator++() {
-        _numenator += _denomenator;
+        _numerator += _denominator;
         return *this;
     }
 
-    Rational &operator--() {
-        _numenator -= _denomenator;
-        return *this;
-    }
-
+    // postfix increment
     Rational operator++(int) {
-        Rational result = *this;
-        ++(*this);
-        return result;
+        Rational old = *this; // copy
+        operator++();
+        return old;
     }
 
+    // prefix decrement
+    Rational &operator--() {
+        _numerator -= _denominator;
+        return *this;
+    }
+
+    // postfix decrement
     Rational operator--(int) {
-        Rational result = *this;
+        Rational result = *this; // copy
         --(*this);
         return result;
     }
 
-    Rational operator+(Rational &other) const {
-        return {_numenator * other._denomenator + _denomenator * other._numenator, _denomenator * other._denomenator};
+    Rational operator+(const Rational &other) const {
+        return {_numerator * other._denominator + _denominator * other._numerator, _denominator * other._denominator};
     }
 
-    Rational operator-(Rational &other) const {
-        return {_numenator * other._denomenator - _denomenator * other._numenator, _denomenator * other._denomenator};
+    Rational operator-(const Rational &other) const {
+        return {_numerator * other._denominator - _denominator * other._numerator, _denominator * other._denominator};
     }
 
-    Rational operator*(Rational &other) const {
-        return {_numenator * other._numenator, _denomenator * other._denomenator};
+    Rational operator*(const Rational &other) const {
+        return {_numerator * other._numerator, _denominator * other._denominator};
     }
 
-    Rational operator/(Rational &other) const {
-        return {_numenator * other._denomenator, _denomenator * other._numenator};
+
+    template<typename T>
+    friend Rational operator*(const T factor, const Rational &other) {
+        return {other._numerator * factor, other._denominator};
     }
 
-    Rational operator+=(Rational &other) {
-        _numenator = _numenator * other._denomenator + _denomenator * other._numenator;
-        _denomenator = _denomenator * other._denomenator;
+    Rational operator/(const Rational &other) const {
+        return {_numerator * other._denominator, _denominator * other._numerator};
+    }
+
+    Rational operator+=(const Rational &other) {
+        _numerator = _numerator * other._denominator + _denominator * other._numerator;
+        _denominator = _denominator * other._denominator;
         return *this;
     }
 
-    Rational operator-=(Rational &other) {
-        _numenator = _numenator * other._denomenator - _denomenator * other._numenator;
-        _denomenator = _denomenator * other._denomenator;
+    Rational operator-=(const Rational &other) {
+        _numerator = _numerator * other._denominator - _denominator * other._numerator;
+        _denominator = _denominator * other._denominator;
         return *this;
     }
 
-    Rational operator*=(Rational &other) {
-        _numenator = _numenator * other._numenator;
-        _denomenator = _denomenator * other._denomenator;
+    Rational operator*=(const Rational &other) {
+        _numerator = _numerator * other._numerator;
+        _denominator = _denominator * other._denominator;
         return *this;
     }
 
-    Rational operator/=(Rational &other) {
-        _numenator = _numenator * other._denomenator;
-        _denomenator = _denomenator * other._numenator;
+    Rational operator/=(const Rational &other) {
+        _numerator = _numerator * other._denominator;
+        _denominator = _denominator * other._numerator;
         return *this;
+    }
+
+
+    friend Rational intPow(Rational r, unsigned long long power) {
+        return {
+                static_cast<NumberType>(BinaryPower(r._numerator, power)),
+                static_cast<NumberType>(BinaryPower(r._denominator, power)),
+        };
     }
 
     friend Rational pow(Rational r, double power) {
-        return {static_cast<int>(pow(r._numenator, power)), static_cast<int>( pow(r._denomenator, power))};
+        return {static_cast<NumberType>(pow(r._numerator, power)),
+                static_cast<NumberType>( pow(r._denominator, power))};
     }
 
-    void reduction() {
-        int t = gcd(_numenator, _denomenator);
-        _numenator /= t;
-        _denomenator /= t;
-    }
 
     explicit operator double() const {
-        return static_cast<double >(_numenator) / static_cast<double >(_denomenator);
+        return static_cast<double >(_numerator) / static_cast<double >(_denominator);
     }
 
-    [[nodiscard]] int numenator() const {
-        return _numenator;
+
+    [[nodiscard]] NumberType numerator() const {
+        return _numerator;
     }
 
-    [[nodiscard]] int denomenator() const {
-        return _denomenator;
+    [[nodiscard]] NumberType denominator() const {
+        return _denominator;
+    }
+
+
+    /// Приводит к несократимому виду
+    void reduce() {
+        NumberType t = gcd(_numerator, _denominator);
+        _numerator /= t;
+        _denominator /= t;
+
+
+        if (_denominator < static_cast<NumberType>(0)) {
+            _denominator = -_denominator;
+            _numerator = -_numerator;
+        }
     }
 
 private:
-    int _numenator, _denomenator;
+    NumberType _numerator, _denominator;
 };
-
 
 void test_rational_number() {
     Rational r, q, p;
@@ -133,35 +183,35 @@ void test_rational_number() {
 
     // RationalNumber(int m, int n)
     r = {1, 2};
-    assert(r.numenator() == 1);
-    assert(r.denomenator() == 2);
+    assert(r.numerator() == 1);
+    assert(r.denominator() == 2);
     r = {-32423423, 234322};
-    assert(r.numenator() == -32423423);
-    assert(r.denomenator() == 234322);
+    assert(r.numerator() == -32423423);
+    assert(r.denominator() == 234322);
 
 
     // explicit RationalNumber(int m)
     r = Rational(1);
-    assert(r.numenator() == 1);
-    assert(r.denomenator() == 1);
+    assert(r.numerator() == 1);
+    assert(r.denominator() == 1);
     r = Rational(23423423);
-    assert(r.numenator() == 23423423);
-    assert(r.denomenator() == 1);
+    assert(r.numerator() == 23423423);
+    assert(r.denominator() == 1);
 
 
     // explicit RationalNumber(double number, double precision)
     r = Rational(1.0, 10.0);
-    assert(r.numenator() == 1);
-    assert(r.denomenator() == 1);
+    assert(r.numerator() == 1);
+    assert(r.denominator() == 1);
     r = Rational(1.234824, 1e7);
-    assert(r.numenator() == 1234824 / 8);
-    assert(r.denomenator() == 125000);
+    assert(r.numerator() == 1234824 / 8);
+    assert(r.denominator() == 125000);
 
 
     // RationalNumber()
     r = Rational();
-    assert(r.numenator() == 0);
-    assert(r.denomenator() == 1);
+    assert(r.numerator() == 0);
+    assert(r.denominator() == 1);
 
 
     // &operator<<
@@ -176,123 +226,123 @@ void test_rational_number() {
     // operator-
     r = {3243, 23};
     q = -r;
-    assert(q.numenator() == -r.numenator());
-    assert(q.denomenator() == r.denomenator());
+    assert(q.numerator() == -r.numerator());
+    assert(q.denominator() == r.denominator());
 
 
     // &operator++
     r = {123, 23};
     q = ++r;
-    assert(r.numenator() == 146);
-    assert(r.denomenator() == 23);
-    assert(q.numenator() == 146);
-    assert(q.denomenator() == 23);
+    assert(r.numerator() == 146);
+    assert(r.denominator() == 23);
+    assert(q.numerator() == 146);
+    assert(q.denominator() == 23);
 
 
     // &operator--
     r = {123, 23};
     q = --r;
-    assert(r.numenator() == 100);
-    assert(r.denomenator() == 23);
-    assert(q.numenator() == 100);
-    assert(q.denomenator() == 23);
+    assert(r.numerator() == 100);
+    assert(r.denominator() == 23);
+    assert(q.numerator() == 100);
+    assert(q.denominator() == 23);
 
     // operator++
     r = {123, 23};
     q = r++;
-    assert(r.numenator() == 146);
-    assert(r.denomenator() == 23);
-    assert(q.numenator() == 123);
-    assert(q.denomenator() == 23);
+    assert(r.numerator() == 146);
+    assert(r.denominator() == 23);
+    assert(q.numerator() == 123);
+    assert(q.denominator() == 23);
 
 
     // operator--
     r = {123, 23};
     q = r--;
-    assert(r.numenator() == 100);
-    assert(r.denomenator() == 23);
-    assert(q.numenator() == 123);
-    assert(q.denomenator() == 23);
+    assert(r.numerator() == 100);
+    assert(r.denominator() == 23);
+    assert(q.numerator() == 123);
+    assert(q.denominator() == 23);
 
 
     // operator+
     r = {123, 23};
     q = {34, 40};
     p = r + q;
-    assert(p.numenator() == 123 * 40 + 23 * 34);
-    assert(p.denomenator() == 23 * 40);
+    assert(p.numerator() == 123 * 40 + 23 * 34);
+    assert(p.denominator() == 23 * 40);
 
 
     // operator-
     r = {123, 23};
     q = {34, 40};
     p = r - q;
-    assert(p.numenator() == 123 * 40 - 23 * 34);
-    assert(p.denomenator() == 23 * 40);
+    assert(p.numerator() == 123 * 40 - 23 * 34);
+    assert(p.denominator() == 23 * 40);
 
 
     // operator*
     r = {123, 23};
     q = {34, 40};
     p = r * q;
-    assert(p.numenator() == 123 * 34);
-    assert(p.denomenator() == 23 * 40);
+    assert(p.numerator() == 123 * 34);
+    assert(p.denominator() == 23 * 40);
 
 
     // operator/
     r = {123, 23};
     q = {34, 40};
     p = r / q;
-    assert(p.numenator() == 123 * 40);
-    assert(p.denomenator() == 23 * 34);
+    assert(p.numerator() == 123 * 40);
+    assert(p.denominator() == 23 * 34);
 
 
     // operator+=
     r = {123, 23};
     q = {34, 40};
     r += q;
-    assert(r.numenator() == 123 * 40 + 23 * 34);
-    assert(r.denomenator() == 23 * 40);
+    assert(r.numerator() == 123 * 40 + 23 * 34);
+    assert(r.denominator() == 23 * 40);
 
 
     // operator-=
     r = {123, 23};
     q = {34, 40};
     r -= q;
-    assert(r.numenator() == 123 * 40 - 23 * 34);
-    assert(r.denomenator() == 23 * 40);
+    assert(r.numerator() == 123 * 40 - 23 * 34);
+    assert(r.denominator() == 23 * 40);
 
 
     // operator*
     r = {123, 23};
     q = {34, 40};
     r *= q;
-    assert(r.numenator() == 123 * 34);
-    assert(r.denomenator() == 23 * 40);
+    assert(r.numerator() == 123 * 34);
+    assert(r.denominator() == 23 * 40);
 
 
     // operator/
     r = {123, 23};
     q = {34, 40};
     r /= q;
-    assert(r.numenator() == 123 * 40);
-    assert(r.denomenator() == 23 * 34);
+    assert(r.numerator() == 123 * 40);
+    assert(r.denominator() == 23 * 34);
 
     // pow
     r = {2, 3};
     q = pow(r, 3);
-    assert(q.numenator() == 8);
-    assert(q.denomenator() == 27);
+    assert(q.numerator() == 8);
+    assert(q.denominator() == 27);
 
     // reduction
     r = {14, 21};
-    r.reduction();
-    assert(r.numenator() == 2);
-    assert(r.denomenator() == 3);
+    r.reduce();
+    assert(r.numerator() == 2);
+    assert(r.denominator() == 3);
     r = {5 * 7 * 763478, 9 * 11 * 763478};
-    r.reduction();
-    assert(r.numenator() == 5 * 7);
-    assert(r.denomenator() == 9 * 11);
+    r.reduce();
+    assert(r.numerator() == 5 * 7);
+    assert(r.denominator() == 9 * 11);
 
     // double
     assert(fabs(static_cast<double >(Rational(1, 2)) - 0.5) < 1e-10);
@@ -301,6 +351,62 @@ void test_rational_number() {
 
 }
 
+template<typename T>
+void printVector(const std::vector<T> &vec, const std::string &sep = ", ", const std::string &term = "\n") {
+
+    std::cout << "{";
+
+    size_t len = vec.size();
+
+    if (len == 0) {
+        std::cout << "}" << term;
+        return;
+    }
+
+    std::cout << vec[0];
+
+    for (size_t i = 1; i < len; ++i) {
+        std::cout << sep << vec[i];
+    }
+
+    std::cout << "}" << term;
+}
+
+
+/// Возвращает первых n чисел Бернулли, начиная с первого
+std::vector<Rational> getBernoulliNumbers(const int n) {
+    std::vector<Rational> numbers(n);
+    std::vector<Rational> buffer(n + 1);
+    buffer[0] = 1;// Делаем нулевую итерация цикла
+
+    for (int i = 1; i <= n; ++i) {
+        buffer[i] = {1, i + 1};
+        for (int j = i; j > 0; --j) {
+            buffer[j - 1] = j * (buffer[j - 1] - buffer[j]);
+            buffer[j - 1].reduce();
+        }
+
+        numbers[i - 1] = buffer[0];
+    }
+
+    return numbers;
+}
+
+
 int main() {
     test_rational_number();
+
+    int max_k = 8;
+
+//    for (int k = 1; k <= max_k; ++k) {
+//        std::cout << "k = " << k << ": ";
+//        auto BernoulliNumbers = getBernoulliNumbers(k);
+//        printVector(BernoulliNumbers);
+//    }
+
+
+    auto BernoulliNumbers = getBernoulliNumbers(2 * max_k);
+    for (int k = 1; k <= max_k; ++k) {
+        std::cout << "B_" << 2 * k << " = " << BernoulliNumbers[2 * k - 1] << std::endl;
+    }
 }
